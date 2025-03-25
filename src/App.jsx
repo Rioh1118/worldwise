cf4588a30525ef9dfe86e93650bbddf9879c50d3
Rioh <90512887+Rioh1118@users.noreply.github.com>
2025-03-25 22:48:16 +0900

    second commit 実質initial commit みたいなもん

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AppLayout from './pages/AppLayout';
 import Homepage from './pages/Homepage';
+import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import Pricing from './pages/Pricing';
 import Product from './pages/Product';
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
