4ad174644c5cd7f339940a00246695a153674a44
Rioh <90512887+Rioh1118@users.noreply.github.com>
2025-03-25 19:09:36 +0900

    PageNavのinitial styling

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,7 +1,8 @@
 import { NavLink } from 'react-router-dom';
+import styles from './PageNav.module.css';
 function PageNav() {
   return (
-    <div>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>
@@ -13,7 +14,7 @@ function PageNav() {
           <NavLink to="/product">Product</NavLink>
         </li>
       </ul>
-    </div>
+    </nav>
   );
 }
 
